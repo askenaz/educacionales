9ce486fd7fe42ee0cdd601be5bc1b2bf3b588d97
dnajeragit <danielnajera@protonmail.com>
2021-09-17 00:20:25 -0300

    first commit

---
+++ src/main/java/com/nur/educacionales/EducacionalesApplication.java
@@ -0,0 +1,13 @@
+package com.nur.educacionales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducacionalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducacionalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nur/educacionales/EducacionalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nur.educacionales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducacionalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
